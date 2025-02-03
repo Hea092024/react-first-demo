6345536fd27adcc33f697608a26e0d0a3c0db0c0
Hector Acevedo <HEA092024@gmail.com>
2025-02-03 12:16:26 +0100

    .

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,3 +1,9 @@
 export default function MyComponent() {
-    
+    return (
+        <div>
+            <h1>Hei fra MyComponent</h1>
+            <p>Dette er en paragraf</p>
+            
+        </div>
+    )
 }
